723d881da2d3a4ca32cab0cda640924d76f0f001
Pear <flippyloveskelly2@gmail.com>
2023-07-16 18:02:33 -0600

    Navmesh facesets are now generated properly

--- NVNode.cs
+++ NVNode.cs
@@ -54,6 +54,12 @@ public sealed class NVNode : GeoNode
                     new VertexPositionColor(vert3Position, facesetColor),
                     new VertexPositionColor(vert2Position, facesetColor)
                 });
+                facesets.AddRange(new[]
+                {
+                    new VertexPositionColor(vert1Position, facesetColor),
+                    new VertexPositionColor(vert2Position, facesetColor),
+                    new VertexPositionColor(vert3Position, facesetColor)
+                });
             }
         }
         Vertices = vertices;
